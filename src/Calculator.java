08dbfe0e4ebb2b673a07e85b72329553265a498a
TDix21 <Tyrese.Dixon25@gmail.com>
2026-02-24 14:10:14 -0500

    Added in multiplication

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,6 +8,10 @@ public class Calculator {
         return a - b;
     }
 
+    public int multiply (int a, int b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
         System.out.println("This is a sample calculator that has no code in its current capacity and I am using this to test a Jenkins pipeline.");
     }
